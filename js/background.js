52b553bab2824b32d1ef46dba3290a63511364a3
moonbs95 <moonbs0318@gmail.com>
2023-04-30 16:19:49 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const images = ['02.jpg', '03.jpg'];
+
+const chosenImage = images[Math.round(Math.random() * (images.length - 1))];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
