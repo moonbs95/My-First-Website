a258d29a95e1acfd17b03d9aa66e830f94265632
moonbs95 <moonbs0318@gmail.com>
2023-04-30 19:06:52 +0900

    Update web design
    1. HTML data structure is changed for CSS file
    2. Many properties are added
    3. Add some hyphen mark to innertext of author in quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -23,4 +23,4 @@ const author = document.querySelector("#quote span:last-child");
 const todaysQuote = quotes[Math.round(Math.random() * (quotes.length - 1))];
 
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+author.innerText = `-${todaysQuote.author}-`;
