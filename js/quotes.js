b255aaa69c64c8ae7850c6e5f42d576d1fa46a04
moonbs95 <moonbs0318@gmail.com>
2023-04-30 16:15:10 +0900

    Create quotes.js

---
+++ js/quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+    {
+        quote: "JUST DO IT",
+        author: "Catchphrase of NIKE",
+    },
+    {
+        quote: "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "If you want to conquer fear, don't sit home and think about it. Go out and get busy.",
+        author: "Dale Carnegie",
+    },
+    {
+        quote: "Life is too short to be little.",
+        author: "Benjamin Disraeli",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.round(Math.random() * (quotes.length - 1))];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysquote.author;
