9c17d61cb7e2bf4694b146151e148cee869ac73e
moonbs95 <moonbs0318@gmail.com>
2023-04-30 17:47:06 +0900

    Update background.js
    Add style attribute to body of HTML, instead of add img tag to body to use background image.

--- js/background.js
+++ js/background.js
@@ -2,7 +2,8 @@ const images = ['02.jpg', '03.jpg'];
 
 const chosenImage = images[Math.round(Math.random() * (images.length - 1))];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+const style = document.createAttribute("style");
+const bgImage = `background-image: url('img/${chosenImage}')`;
+style.value = bgImage;
 
-document.body.appendChild(bgImage);
+document.body.setAttributeNode(style);
